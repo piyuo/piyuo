08b0252a4161c32b6d69c2a84f2ca799ff233e19
piyuo <piyuo.com@gmail.com>
2025-03-17 18:00:52 -0700

    feat: add margin property to GlassContainer for improved layout flexibility

--- lib/index/src/glass_container.dart
+++ lib/index/src/glass_container.dart
@@ -3,13 +3,21 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class GlassContainer extends StatelessWidget {
-  const GlassContainer({required this.child, this.padding = const EdgeInsets.fromLTRB(20, 10, 20, 10), super.key});
+  const GlassContainer({
+    required this.child,
+    this.padding = const EdgeInsets.fromLTRB(20, 10, 20, 10),
+    this.margin = EdgeInsets.zero,
+    super.key,
+  });
 
   /// the child widget to be displayed in the glass container
   final Widget child;
 
   /// the padding of the glass container
   final EdgeInsetsGeometry padding;
+
+  /// the margin of the glass container
+  final EdgeInsetsGeometry margin;
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +27,11 @@ class GlassContainer extends StatelessWidget {
         filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
         child: Container(
           padding: padding,
+          margin: margin,
           decoration: BoxDecoration(
-            color: Colors.white.withValues(alpha: 0.2), // 半透明背景
-            borderRadius: BorderRadius.circular(20), // 圓角
-            border: Border.all(color: Colors.white.withValues(alpha: 0.3)), // 白色邊框
+            color: Colors.white.withValues(alpha: 0.2),
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.white.withValues(alpha: 0.3)),
           ),
           alignment: Alignment.center,
           child: child,
